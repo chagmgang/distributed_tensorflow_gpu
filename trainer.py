72905509ecfc33ef2dcaca6169be36a1f2496711
chagmgang <chagmgang@gmail.com>
2020-01-02 15:30:35 +0900

    branch

--- trainer.py
+++ trainer.py
@@ -25,6 +25,14 @@ def pin_global_variables(device):
     with tf.variable_scope('', custom_getter=getter) as vs:
         yield vs
 
+def model():
+    ph = tf.placeholder(tf.float32, shape=[None, 4])
+    result = tf.layers.dense(inputs=ph, units=3, activation=tf.nn.softmax)
+    return ph, result
+
+def build_learner(result):
+    return tf.layers.dense(inputs=result, units=2, activation=tf.nn.softmax)
+
 def main(_):
     num_actors = 1
     task = 0
@@ -32,6 +40,7 @@ def main(_):
     shared_job_device = '/job:learner/task:0'
     is_actor_fn = lambda i: FLAGS.job_name == 'actor' and i == task
     is_learner = FLAGS.job_name == 'learner'
+    filters = [shared_job_device, local_job_device]
 
     global_variable_device = shared_job_device + '/cpu'
 
@@ -40,48 +49,89 @@ def main(_):
         'learner': ['localhost:8000']})
 
     server = tf.train.Server(cluster, job_name=FLAGS.job_name, task_index=task)
+    
+    with tf.device(local_job_device):
 
-    with tf.Graph().as_default(), \
-         tf.device(local_job_device), \
-         pin_global_variables(global_variable_device):
-
-        ## build agent
-        with tf.device(shared_job_device):
-            with tf.device('/cpu'):
-                ph = tf.placeholder(tf.float32, shape=[None, 4])
-                l1 = tf.layers.dense(inputs=ph, units=256, activation=tf.nn.relu)
-                l2 = tf.layers.dense(inputs=l1, units=3, activation=tf.nn.softmax)
-
-        ## build learner
         if is_learner:
-            ph = tf.placeholder(tf.float32, shape=[None, 4])
-            l1 = tf.layers.dense(inputs=ph, units=256, activation=tf.nn.relu)
-            l2 = tf.layers.dense(inputs=l1, units=3, activation=tf.nn.softmax)
+            with tf.device(shared_job_device):
+                shared_input = tf.placeholder(tf.float32, shape=[None, 28, 28, 1])
+                shared_conv1 = tf.layers.conv2d(
+                    inputs=shared_input, filters=32, kernel_size=[5, 5], strides=[2, 2],
+                    padding='valid', activation=tf.nn.relu)
+                shared_conv2 = tf.layers.conv2d(
+                    inputs=shared_conv1, filters=32, kernel_size=[5, 5], strides=[2, 2],
+                    padding='valid', activation=tf.nn.relu)
+                shared_flatten = tf.layers.flatten(shared_conv2)
+                shared_flatten = tf.layers.dense(inputs=shared_flatten, units=256, activation=tf.nn.relu)
+                shared_flatten = tf.layers.dense(inputs=shared_flatten, units=256, activation=tf.nn.relu)
+                shared_flatten = tf.layers.dense(inputs=shared_flatten, units=256, activation=tf.nn.relu)
+                shared_flatten = tf.layers.dense(inputs=shared_flatten, units=256, activation=tf.nn.relu)
+                shared_predict = tf.layers.dense(inputs=shared_flatten, units=10, activation=tf.nn.softmax)
 
-        sess = tf.Session(server.target)
-        sess.run(tf.global_variables_initializer())
+                shared_label = tf.placeholder(tf.int32, shape=[None])
+                shared_onehot = tf.one_hot(shared_label, 10)
+
+                shared_cross_entropy = tf.reduce_mean(-shared_onehot * tf.log(shared_predict + 1e-8))
+                shared_train = tf.train.AdamOptimizer(0.00001).minimize(shared_cross_entropy)
+        else:
+            with tf.device('/cpu'):
+                local_input = tf.placeholder(tf.float32, shape=[None, 28, 28, 1])
+                local_conv1 = tf.layers.conv2d(
+                    inputs=local_input, filters=32, kernel_size=[5, 5], strides=[2, 2],
+                    padding='valid', activation=tf.nn.relu)
+                local_conv2 = tf.layers.conv2d(
+                    inputs=local_conv1, filters=32, kernel_size=[5, 5], strides=[2, 2],
+                    padding='valid', activation=tf.nn.relu)
+                local_flatten = tf.layers.flatten(local_conv2)
+                local_predict = tf.layers.dense(inputs=local_flatten, units=10, activation=tf.nn.softmax)
+
+                local_label = tf.placeholder(tf.int32, shape=[None])
+                local_onehot = tf.one_hot(local_label, 10)
+
+                local_cross_entropy = tf.reduce_mean(-local_onehot * tf.log(local_predict + 1e-8))
+                local_train = tf.train.AdamOptimizer(0.00001).minimize(local_cross_entropy)
+
+
+    session = tf.Session(server.target)
+    session.run(tf.global_variables_initializer())
+
+    ((train_data, train_labels),
+        (eval_data, eval_labels)) = tf.keras.datasets.mnist.load_data()
+
+    train_data = train_data/np.float32(255)
+    train_data = np.expand_dims(train_data, axis=3)
+    train_labels = train_labels.astype(np.int32)
+
+    batch_size = 50
+    data_size = len(train_data)
+    epoch_size = int(data_size / batch_size)
+
+    start_time = time.time()
 
     if is_learner:
-        while True:
-            start_time = time.time()
-            sess.run(
-                l2,
-                feed_dict={ph: [[1,2,3,4]]})
-            end_time = time.time()
-
-            print('gpu operation time : {}'.format(end_time - start_time))
-            print('######')
-
+        for i in range(epoch_size):
+            batch_data = train_data[i*batch_size : (i+1)*batch_size]
+            batch_label = train_labels[i*batch_size : (i+1)*batch_size]
+            loss, _ = session.run(
+                [shared_cross_entropy, shared_train],
+                feed_dict={shared_input: batch_data, shared_label: batch_label})
+            print(loss)
+    
     else:
         while True:
-            start_time = time.time()
-            sess.run(
-                l2,
-                feed_dict={ph: [[1,2,3,4]]})
-            end_time = time.time()
+            print('x')
+        # for i in range(epoch_size):
+        #     batch_data = train_data[i*batch_size : (i+1)*batch_size]
+        #     batch_label = train_labels[i*batch_size : (i+1)*batch_size]
+        #     loss, _ = session.run(
+        #         [local_cross_entropy, local_train],
+        #         feed_dict={local_input: batch_data, local_label: batch_label})
+        #     print(loss)
 
-            print('cpu operation time : {}'.format(end_time - start_time))
-            print('######')
+    end_time = time.time()
+
+    print(end_time - start_time)
+        
 
 if __name__ == '__main__':
     tf.app.run()
